4a20e338e795b2e9783e060b6508fd02534c3379
Oscar702 <FunesLV@aol.com>
2020-12-20 00:57:39 +0000

    almost

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  function randomSuits() {
+    return suits[Math.floor(Math.random() * suits.length)];
+  }
+  function randomNumbers() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  }
+  var design = randomSuits();
+
+  if (design == "&hearts;" || design == "&diams;") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+  }
+
+  document.querySelector(".top-suit").innerHTML = design;
+  document.querySelector(".bottom-suit").innerHTML = design;
+  document.querySelector(".number").innerHTML = randomNumbers();
 };
